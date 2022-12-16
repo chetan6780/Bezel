9393bea536544bd86baf7bca76625b371e50123a
chetan6780 <crp20802@gmail.com>
2022-12-16 20:49:01 +0530

    redeploy

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-// const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
+const sockets = io('https://bezel-rtc.vercel.app/', { autoConnect: true, forceNew: true });
 // connect to the server
-const sockets = io('/');
+// const sockets = io('/');
 export default sockets;
